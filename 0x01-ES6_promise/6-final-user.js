f2c47c21f61bd5282e863e5f9dc2d63f84879091
Mythamor <bethmithamo.bm@gmail.com>
2023-11-30 00:30:20 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,20 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignUp(firstName, lastName, fileName) {
-  const userPromise =  signUpUser(firstName, lastName);
+  const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
   return Promise.all([userPromise, photoPromise])
-    .then((results) => {
-      return results.map((result) => ({
-        status: 'resolved',
-        value: result,
-      }));
-    })
-    .catch((error) => {
-      return [{
-        status: 'rejected',
-        value: error,
-      }];
-    });
+    .then((result) => ({
+      status: 'resolved',
+      value: result,
+    }))
+    .catch((error) => [{
+      status: 'rejected',
+      value: error,
+    }]);
 }
