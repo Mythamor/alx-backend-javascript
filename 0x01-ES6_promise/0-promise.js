75cdf58af2c461a6a05c1aa63c6a5fa5ff68cbef
Mythamor <bethmithamo.bm@gmail.com>
2023-11-29 20:01:52 +0300

    Promises in JS

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("true");
+      resolve('true');
     }, 250);
   });
 }
