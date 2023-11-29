818f39964c3f3647375438b53687a91112fa2ab9
Mythamor <bethmithamo.bm@gmail.com>
2023-11-29 22:00:33 +0300

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      const { body: photoBody } = photoResponse;
+      const { firstName, lastName } = userResponse;
+
+      console.log(`${photoBody} ${firstName} ${lastName}`);
+    })
+    .catch((error) => {
+      console.error('', error);
+    });
+}
+
+export default handleProfileSignup;
