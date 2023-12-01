5d8c67edf4f6a0a38c248f36e9b6669edbccfd92
Mythamor <bethmithamo.bm@gmail.com>
2023-12-01 20:26:43 +0300

    Class as an abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,27 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = 0;
-
-    this.sqft = sqft;
-
+  // Abstrat method: subclasses implementation
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
   }
 
   // Getter and setter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
-    this._sqft = value;
-  }
-
-  // Abstrat method: subclasses implementation
-  evacuationWarningMessage() {
-     if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-       throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
   }
 }
