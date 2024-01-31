ea4bd2cfc5948a8132961e635bfdbf6e8f47b751
Mythamor <bethmithamo.bm@gmail.com>
2024-01-31 17:59:02 +0300

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
 const http = require('http');
+
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.setHeader(200, {'Content-Type': 'text/plain' });
+  res.setHeader(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!\n');
 });
 
