91189db8a4d5f71002eae39f650ebd7e80878f31
Mythamor <bethmithamo.bm@gmail.com>
2024-01-31 18:59:02 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,23 +4,24 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Read user input from stdin
-const handleInput = (input) => {
-  const name = input.toString();
-
-  // Display user's name
-  process.stdout.write(`Your name is: ${name}`);
-
-  if (!process.stdin.isTTY) {
-    process.exit();
-  }
-};
-
-// Check if there's a piped input
 if (process.stdin.isTTY) {
-  process.stdin.once('data', handleInput);
-} else {
-  process.stdin.on('data', handleInput);
+  process.stdin.on('data', (data) => {
+    const name = data.toString();
 
+    // Display user's name
+    process.stdout.write(`Your name is: ${name}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    const name = data.toString();
+
+    // Display user's name
+    process.stdout.write(`Your name is: ${name}`);
+    process.exit();
+  });
+
+  // Set up listener for process exit event
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
   });
