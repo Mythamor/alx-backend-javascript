e29a6fd0f95f7d25ec1ba2f8516c79e588c92528
Mythamor <bethmithamo.bm@gmail.com>
2024-02-02 00:33:58 +0300

    fix test error

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,15 @@
 const request = require('supertest');
+const { expect } = require('chai');
 const app = require('./api');
 
 describe('Index page', () => {
   it('should return status code 200', async () => {
     const response = await request(app).get('/');
-    expect(response.status).toEqual(200);
+    expect(response.status).to.equal(200);
   });
 
   it('should return "Welcome to the payment system"', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toEqual('Welcome to the payment system')
+    expect(response.text).to.equal('Welcome to the payment system')
   });
 });
